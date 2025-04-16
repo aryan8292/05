831bfe56903caf742268fe119a2e417b12c58a5c
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2025-04-16 09:05:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7779663426:AAFBUIRmPsuTFY9jU94v4O5_QsEQmQ4
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/LTp.jpg')).split()
 
 
 # Admins & Users
